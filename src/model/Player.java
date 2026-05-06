b54ce0b80a64372b0ab672636f433ddc7f3d0adc
Anas <123456789+anasarbay-codes@users.noreply.github.com>
2026-05-06 15:47:48 +0100

    change

--- src/model/Player.java
+++ src/model/Player.java
@@ -10,6 +10,7 @@ public class Player {
     }
 
     public String getName() { return name; }
+    //public void setName(String name) { this.name = name; }
     public int getScore() { return score; }
     public void setScore(int score) { this.score = score; }
 }
